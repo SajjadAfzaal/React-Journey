24f10fd7da48fbc6319b96dc5f372c0d90d679dc
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-12 15:31:34 +0500

    1st jsx snippet

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -20,6 +20,10 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello, React!</h1>);
+// root.render(<h1>Hello, React!</h1>);
+const createElement = <h1>Hello from JSX!</h1>;
 
+console.log(createElement);
+
+root.render(createElement);
 export default App;
