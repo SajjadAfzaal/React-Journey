78b5e8d9a620db95b40fb32a182ce64dea8d83b1
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-12 14:46:34 +0500

    1st react code

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -4,14 +4,22 @@
 // var root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<h1>Hello React!</h1>);
 
-import React from "react";
+// import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello React with Vite!</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello React with Vite!</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 1st react code
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>Hello, React!</h1>);
 
 export default App;
