ba4d3c819f65083e4e70e99129af43d350cb7ab6
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-15 17:25:46 +0500

    Organized components

---
+++ react-crash-course/src/Header.jsx
@@ -0,0 +1,16 @@
+import logo from "./react-logo.png";
+
+export default function Header() {
+  return (
+    <header className="head">
+      <img className="nav-logo" src={logo} alt="React logo" />
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Pricing</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
